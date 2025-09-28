fb23362c91b7e64d7f89dfee2b2f78690f26ebad
tinytinus <tinyducky1@proton.me>
2025-09-28 11:47:48 +0200

    no longer using polybar; still here for archiving

--- scripts/toggle_status.sh
+++ scripts/toggle_status.sh
@@ -5,11 +5,12 @@ if pgrep -f "floating-status" > /dev/null; then
     pkill -f "floating-status"
 else
     # Start the status window
-    kitty --class=floating-status \
-          -o initial_window_width=320 \
-          -o initial_window_height=140 \
-					-o font_size=12 \
-          -o remember_window_size=no \
-          -o window_padding_width=10 \
-          ~/bin/bspwm/status &
+		alacritty --class=floating-status \
+          --option window.dimensions.columns=32 \
+          --option window.dimensions.lines=5 \
+          --option font.size=10 \
+          --option window.padding.x=10 \
+          --option window.padding.y=6 \
+          --option window.dynamic_padding=false \
+          -e ~/bin/bspwm/status &
 fi
